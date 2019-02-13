1e7548c566ac83a52ee4e0a31d3fd38118d7c784
ClaytoPro <claytonprovan@gmail.com>
2019-02-13 11:05:07 -0500

    module 2, needs more testing doe

---
+++ a2/HelperFunction.h
@@ -0,0 +1,28 @@
+/*  by:Clayton Provan
+*   functioned used to help CalendarParser.c
+*/
+
+
+
+#ifndef HELPERFUNCTION_H
+#define HELPERFUNCTION_H
+
+#include <ctype.h>
+/*
+turns a string into property struct
+returns pointer to that property.
+*/
+ICalErrorCode addProperty(char *property, Calendar** obj);
+
+ICalErrorCode addPropertyEvent(char *property, Event** obj);
+
+ICalErrorCode addPropertyAlarm(char *property, Alarm** obj);
+
+//convert a int to a char
+char dateBoolToChar(int z);
+/*check if DateTime String is valid */
+int isValidDateTime(DateTime obj);
+//check if input property follows standard
+int isValidProperty(Property *obj);
+
+#endif
---
+++ a2/test.c
@@ -0,0 +1,124 @@
+/** Test cases for claendar calendar parser
+*   By:Clayton Provan
+*
+**/
+
+#include "CalendarParser.c"
+#include "LinkedListAPI.c"
+
+
+
+int main(){
+  Calendar *tester = NULL;
+  char *calendarToPtr;
+
+
+  if(createCalendar("test.ics", &tester) != OK){
+  //printf("error:%s:\n",printError(createCalendar("test.ics", &tester)) );
+
+  }else{
+
+    if(tester != NULL){
+      calendarToPtr = printCalendar(tester);
+      printf("%s\n",calendarToPtr);
+
+      if(calendarToPtr != NULL){
+        //  printf("Freeing calendarString\n" );
+        free(calendarToPtr);
+      }
+      deleteCalendar(tester);
+    }
+  }
+
+  if(createCalendar("test1.ics", &tester) != OK){
+    if(tester != NULL){
+      printf("calendar not set to null on erro\n");
+    }
+    printf("test1:baddd\n");
+  //  deleteCalendar(tester);
+  }else{
+
+    if(tester != NULL){
+      calendarToPtr = printCalendar(tester);
+      printf("%s\n",calendarToPtr);
+
+      if(calendarToPtr != NULL){
+        //  printf("Freeing calendarString\n" );
+        free(calendarToPtr);
+      }
+      deleteCalendar(tester);
+    }
+  }
+
+  if(createCalendar("test2.ics", &tester) != OK){
+    if(tester != NULL){
+      printf("calendar not set to null on erro\n");
+    }
+    printf("test2:baddd\n");
+  //  deleteCalendar(tester);
+  }else{
+
+    if(tester != NULL){
+      calendarToPtr = printCalendar(tester);
+      printf("%s\n",calendarToPtr);
+
+      if(calendarToPtr != NULL){
+        //  printf("Freeing calendarString\n" );
+        free(calendarToPtr);
+      }
+      deleteCalendar(tester);
+    }
+  }
+
+
+    if(createCalendar("test3.ics", &tester) != OK){
+      if(tester != NULL){
+        printf("calendar not set to null on erro\n");
+      }
+      printf("test3:baddd\n");
+    //  deleteCalendar(tester);
+    }else{
+
+      if(tester != NULL){
+        calendarToPtr = printCalendar(tester);
+        printf("%s\n",calendarToPtr);
+
+        //change some shit and test validate
+        if(validateCalendar(tester) == OK) printf("VALIDATE CALENDAR:ok\n");
+        tester->version =0;//kind of a shitty test
+        if(validateCalendar(tester) != OK) printf("NOT OK VALIDATE CALENDAR:ok\n");
+
+
+
+        if(calendarToPtr != NULL){
+          //  printf("Freeing calendarString\n" );
+          free(calendarToPtr);
+        }
+        deleteCalendar(tester);
+      }
+    }
+
+
+      if(createCalendar("test4.ics", &tester) != OK){
+        if(tester != NULL){
+          printf("calendar not set to null on erro\n");
+        }
+        printf("test4:baddd\n");
+      //  deleteCalendar(tester);
+      }else{
+
+        if(tester != NULL){
+          calendarToPtr = printCalendar(tester);
+          printf("%s\n",calendarToPtr);
+          writeCalendar("test5.ics",tester);
+          if(calendarToPtr != NULL){
+            //  printf("Freeing calendarString\n" );
+            free(calendarToPtr);
+          }
+          deleteCalendar(tester);
+        }
+      }
+
+
+  return 0;
+}
